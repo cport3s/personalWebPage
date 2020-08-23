97894924693849a01e62d72c0eb19d94721fc0bf
Carlos Portes <xanozuke@gmail.com>
2020-08-23 14:37:27 -0400

    2020082303
    Added:
    -JS File
    -CSS fade in animation on body
    -Fonts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # personalWebPage
  
+20200823: This is a showcase example using my CV and other personal information.
---
+++ main.js
@@ -0,0 +1,4 @@
+function fadeInBody() {
+    document.body.style.animationName = 'fadeInAnimation';
+    document.body.style.animationDuration = '2s';
+}
