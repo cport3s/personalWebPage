98b06fe3adc4823abad4984d4eb25249923aadb0
Carlos Portes <xanozuke@gmail.com>
2020-08-24 22:43:26 -0400

    2020082401
    Playing with CSS animations, trying to get a collapsable section element.

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 function fadeInBody() {
     document.body.style.animationName = 'fadeInAnimation';
     document.body.style.animationDuration = '2s';
+    var personalInformation = document.getElementsByClassName('personal_information');
+    personalInformation[0].style.animationName = 'expandAnimation';
+    personalInformation[0].style.animationDuration = '4s';
+
 }
