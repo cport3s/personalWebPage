87360a36a59df0b762d81e8c984aecf6fbe9de15
Carlos Portes <xanozuke@gmail.com>
2020-08-27 21:23:49 -0400

    2020082701
    Button added. Changing laptop.

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 function fadeInBody() {
     document.body.style.animationName = 'fadeInAnimation';
     document.body.style.animationDuration = '2s';
-    var personalInformation = document.getElementsByClassName('personal_information');
-    personalInformation[0].style.animationName = 'expandAnimation';
-    personalInformation[0].style.animationDuration = '4s';
 
+    /*var personalInformation = document.getElementsByClassName('personal_information');*/
+    /*personalInformation[0].style.animationName = 'expandAnimation';*/
+    /*personalInformation[0].style.animationDuration = '1s';*/
+}
+
+function showPersonalInfoBox() {
+    
 }
