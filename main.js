a19a7344e6c95c3a96b2321c7b107626f261d005
Carlos Portes <carlosportes.c@gmail.com>
2020-08-28 07:21:51 -0400

    2020082802
    -Removed flex order
    -Changed button height
    -All flex items with information are now collapsable

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
 function fadeInBody() {
     document.body.style.animationName = 'fadeInAnimation';
     document.body.style.animationDuration = '2s';
-
-    /*var personalInformation = document.getElementsByClassName('personal_information');*/
-    /*personalInformation[0].style.animationName = 'expandAnimation';*/
-    /*personalInformation[0].style.animationDuration = '1s';*/
 }
 
 function showInfoBox(classToToggle) {
